c2c1f0b30c53cddb4ae25bcb57a1217a258acfaf
juandiego2009 <zambranojuandiego4@gmail.com>
2026-08-06 13:24:11 -0500

    ejercicio 3 terminado

--- src/Ejercicio3.java
+++ src/Ejercicio3.java
@@ -14,6 +14,16 @@ public class Ejercicio3 {
                 int mayor = numeros[0];
                 int menor = numeros[0];
 
+                for (int i = 1; i < n; i++) {
+                    if (numeros[i] > mayor) {
+                        mayor = numeros[i];
+                    }
+                    if (numeros[i] < menor) {
+                        menor = numeros[i];
+                    }
+                }
+
+                JOptionPane.showMessageDialog(null, "Valor mayor: " + mayor + "\nValor menor: " + menor);
             }
         }
 
