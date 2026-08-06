26203eee21b96fe1eda3fe9492266b813a23c3a0
juandiego2009 <zambranojuandiego4@gmail.com>
2026-08-06 14:57:11 -0500

    ejercicio 4 terminado

--- src/Ejercicio4.java
+++ src/Ejercicio4.java
@@ -13,6 +13,17 @@ public class Ejercicio4 {
             temperaturas[i] = Integer.parseInt(JOptionPane.showInputDialog("ingrese el valor de cada temperatura"));
 
 
-        }
+            if (temperaturas[i] < 10) {
+                resultados += temperaturas[i] + "°C = frio\n";
+
+            } else if (temperaturas[i] > 10 && temperaturas[i] <= 25) {
+                resultados += temperaturas[i] + "°C = templado\n";
+
+            } else {
+                resultados += temperaturas[i] + "°C = caluroso\n";
+            }
+}
+            JOptionPane.showMessageDialog(null, "temperaturas registradas \n \n " + resultados);
+
     }
 }
